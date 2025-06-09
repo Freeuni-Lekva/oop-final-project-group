c449d12216f7af8f8bd4d0510e2d8fddac9e42fe
MerabiRizhamadze <mrizh18@freeuni.edu.ge>
2025-06-09 12:27:34 +0400

    Build: Maven Archetype Webapp initial empty build, using tomcat-maven-plugin and maven archetype build.

---
+++ src/test/java/Junit_test.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit_test {
+
+    @Test
+    public void test_1() {
+        assertEquals(5, 5);
+    }
+}
+
