4271264abbe6465e2c9305dde4f3bf87ce89d989
gorgo <lagorg22@freeuni.edu.ge>
2025-06-22 21:44:19 +0400

    Remove User table SQL script since db schema is centralized

--- src/main/resources/User.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(64) NOT NULL,
-                       password VARBINARY(256),
-                       salt VARBINARY(256)
-);
