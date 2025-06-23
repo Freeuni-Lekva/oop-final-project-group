f16a8faa94a9d68508783892c540925089c42742
Jana <janashiasandro888@gmail.com>
2025-06-24 01:21:46 +0400

    Implement MultipleChoiceQuestionDao with tests

---
+++ src/main/java/dao/MultipleChoiceDao.java
@@ -0,0 +1,60 @@
+package dao;
+
+import models.MultipleChoiceQuestion;
+import models.Question;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Data access object for multiple choice questions.
+ * Handles database operations for questions with predefined options
+ * where users select from available choices.
+ * Uses the "AnswerOptions" table to store options with
+ * their correctness flags.
+ */
+
+public class MultipleChoiceDao extends AbstractQuestionDao {
+
+    @Override
+    protected Object getAnswersFromDB(Connection connection, int questionId) throws SQLException {
+        Map<String, Boolean> options = new HashMap<>();
+        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM AnswerOptions WHERE question_id = ?")){
+            preparedStatement.setInt(1, questionId);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                while (resultSet.next()) {
+                    String answerText = resultSet.getString("option_text");
+                    Boolean isCorrect = resultSet.getBoolean("is_correct");
+                    options.put(answerText, isCorrect);
+                }
+            }
+        }
+        return options;
+    }
+
+    @Override
+    protected void insertAnswersIntoDB(int questionId, Question question, Connection connection) throws SQLException {
+        MultipleChoiceQuestion multipleChoiceQuestion = (MultipleChoiceQuestion)question;
+        Map<String, Boolean> options = multipleChoiceQuestion.getOptions();
+        String query = "INSERT INTO AnswerOptions (question_id, option_text, is_correct) VALUES (?, ?, ?)";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            for(String answer : options.keySet()) {
+                preparedStatement.setInt(1, questionId);
+                preparedStatement.setString(2, answer);
+                preparedStatement.setBoolean(3, options.get(answer));
+                preparedStatement.executeUpdate();
+            }
+        }
+    }
+
+    @Override
+    protected Question createQuestionObject(int questionId, String questionText, Object answers, int quizId, int orderInQuiz){
+        return new MultipleChoiceQuestion(questionId, questionText, (Map<String, Boolean>)answers, quizId, orderInQuiz);
+    }
+
+    @Override
+    protected String getAnswerTableName() {
+        return "AnswerOptions";
+    }
+}
